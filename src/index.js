9a3efecc22cce57bf70c4ad72dacc0141c90146e
Fayaz <fayazfai987@gmail.com>
2025-07-29 12:37:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Portfolio from './App';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Portfolio />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
